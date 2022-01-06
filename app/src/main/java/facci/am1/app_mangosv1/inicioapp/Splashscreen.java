aa4788cd639d34fbd424de95ce63ee602bb6bc75
SantiagoMoreira3C <sanmopa2000@gmail.com>
2022-01-06 15:00:24 -0500

    splash screen completado

---
+++ app/src/main/java/facci/am1/app_mangosv1/inicioapp/Splashscreen.java
@@ -0,0 +1,35 @@
+package facci.am1.app_mangosv1.inicioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import facci.am1.app_mangosv1.MainActivity;
+import facci.am1.app_mangosv1.R;
+
+public class Splashscreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        };
+         Timer tiempo = new Timer();
+         tiempo.schedule(tarea, 5000);
+
+    }
+}
